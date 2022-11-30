c85fb1154395488cd795133dc1798bfc215c1945
Dmitry <mitolbir@gmail.com>
2022-11-30 11:28:33 +0300

    Max from 3 programm ready

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -16,3 +16,34 @@ else
 }
 */
 
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*
+Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+if (num2 > max)
+{
+    max = num2;
+}
+else
+{
+    max = num1;
+}
+if (num3 > max)
+{
+    max = num3;
+}
+else
+{
+    max = num1;
+}
+
+Console.Write("Your max number is: " + max);
+*/
+

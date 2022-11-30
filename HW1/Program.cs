bcb80d9a9e4cb890ce87f564a249233bf7675b7f
Dmitry <mitolbir@gmail.com>
2022-11-30 11:48:37 +0300

    Even/not even programm is ready

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -47,3 +47,20 @@ else
 Console.Write("Your max number is: " + max);
 */
 
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+/*
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Your number is even");
+}
+else
+{
+    Console.WriteLine("Your number not even");
+}
+*/
+

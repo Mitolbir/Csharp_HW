eeb2b0efbcf8de1fa2bb8566e765bb4a08f535a5
Dmitry <mitolbir@gmail.com>
2022-11-30 11:10:18 +0300

    Bigger/smaller programm ready

---
+++ HW1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Домашнее задание 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+/*
+Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("First number is bigger");
+}
+else
+{
+    Console.WriteLine("Second number is bigeer");
+}
+*/
+

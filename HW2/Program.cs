bb174ee50405a3642ab07a3f6844a7237bbb98c6
Dmitry <mitolbir@gmail.com>
2022-12-05 17:35:17 +0300

    Three-digit to third number programm added

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -17,5 +17,47 @@ int newNum = SecondNum(number);
 Console.WriteLine($"New version of a number {number} is {newNum}");
 
 Напишите программу, которая выводит третью цифру заданного числа 
-или сообщает, что третьей цифры нет. */
+или сообщает, что третьей цифры нет. 
+
+int ThirdNum(int num)
+{
+    if (num < 100)
+    {
+        num = -1;
+    }
+    else
+    {
+        if (num < 1000)
+        {
+            num = num % 10;
+        }
+        else
+        {
+        while (num > 999)
+        {
+            num = num / 10;
+        }
+        if (num < 1000)
+        {
+            num = num % 10;
+        }
+        }
+    }
+    return num;
+}
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int newNum = ThirdNum(number);
+
+if (newNum == -1)
+{
+    Console.WriteLine("There is no third digit in your number");
+}
+else
+{
+    Console.WriteLine($"Your third-digit number is {newNum}");
+}
+*/
 

a1ceec14c9115b1293de0c54ac61ef14376f3e68
Dmitry <mitolbir@gmail.com>
2022-12-05 16:16:32 +0300

    Three-digit to second number programm added

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -1,2 +1,21 @@
-﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. */
+﻿/* Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+
+int SecondNum(int num)
+{
+
+    int sec = num % 100 / 10;
+
+    return sec;
+}
+
+Console.Write("Input three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int newNum = SecondNum(number);
+
+Console.WriteLine($"New version of a number {number} is {newNum}");
+
+Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет. */
 
